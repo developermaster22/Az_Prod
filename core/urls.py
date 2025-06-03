44acc9cb4f96872cfc8731850515857245756259
Cesar Linares <cesarlinares1522@gmail.com>
2025-06-03 15:34:57 -0300

    urls fix core

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.views.generic import RedirectView  # 👈 importa esto
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', RedirectView.as_view(url='/dashboard/', permanent=False)),  # 👈 esta línea es nueva
-    path('', include('pedidos.urls')),
+    path("admin/", admin.site.urls),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(next_page="login"),
+        name="logout",
+    ),
+    path("", include("pedidos.urls")),  # Se mantiene al final para que tome prioridad
 ]
