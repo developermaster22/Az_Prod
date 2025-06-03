25746cff9fb64f5c7f062715331564c47d9eaf05
Cesar Linares <cesarlinares1522@gmail.com>
2025-06-03 15:26:32 -0300

    fix login

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,18 +1,36 @@
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LogoutView
+from .views import (
+    CustomLoginView,
+    lista_pedidos,
+    detalle_pedido,
+    crear_pedido,
+    agregar_seguimiento,
+    dashboard,
+    procesar_pedido,
+    vista_administradores,
+    vista_designers,
+    vista_enteladores,
+    vista_impresores,
+    vista_embolsadores,
+)
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-
-    path('pedido/<str:codigo>/', views.detalle_pedido, name='detalle_pedido'),
-    path('pedido/crear/', views.crear_pedido, name='crear_pedido'),
-    path('pedido/<str:codigo>/procesar/', views.procesar_pedido, name='procesar_pedido'),
-    path('pedido/<str:codigo>/agregar-seguimiento/', views.agregar_seguimiento, name='agregar_seguimiento'),
-
-    # Vistas por roles (opcional, según tu código)
-    path('vista/administradores/', views.vista_administradores, name='vista_administradores'),
-    path('vista/disenadores/', views.vista_designers, name='vista_designers'),
-    path('vista/enteladores/', views.vista_enteladores, name='vista_enteladores'),
-    path('vista/impresores/', views.vista_impresores, name='vista_impresores'),
-    path('vista/embolsadores/', views.vista_embolsadores, name='vista_embolsadores'),
+    path("accounts/login/", CustomLoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("", dashboard, name="dashboard"),
+    path("pedidos/", lista_pedidos, name="lista_pedidos"),
+    path("pedidos/crear/", crear_pedido, name="crear_pedido"),
+    path("pedidos/<str:codigo>/", detalle_pedido, name="detalle_pedido"),
+    path(
+        "pedidos/<str:codigo>/agregar_seguimiento/",
+        agregar_seguimiento,
+        name="agregar_seguimiento",
+    ),
+    path("pedidos/<str:codigo>/procesar/", procesar_pedido, name="procesar_pedido"),
+    path("vista_administradores/", vista_administradores, name="vista_administradores"),
+    path("vista_designers/", vista_designers, name="vista_designers"),
+    path("vista_enteladores/", vista_enteladores, name="vista_enteladores"),
+    path("vista_impresores/", vista_impresores, name="vista_impresores"),
+    path("vista_embolsadores/", vista_embolsadores, name="vista_embolsadores"),
 ]
